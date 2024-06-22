ba582df3b5d98be6d4a1ee99b394f57a8d8e0841
Wanderson <wanderbreno2@gmail.com>
2024-06-21 22:33:42 -0300

    atualizar

---
+++ tipos-variaveis/src/relacionais.java
@@ -0,0 +1,31 @@
+public class relacionais {
+    public static void main(String[] args) {
+        String nomeUm = "WANDERSON";
+        String nomeDois = new String("WANDERSON");
+       
+        //System.out.println(nomeUm == nomeDois);
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 < numero2){
+            System.out.println("a nossa condição é verdadeira");
+        }
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual numeroDois? " + simNao);
+        
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente do numeroDois? " + simNao);
+        
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior do que o numeroDois? " + simNao);
+
+        
+
+    }
+
+}
---
+++ tipos-variaveis/src/ternario.java
@@ -0,0 +1,22 @@
+public class ternario {
+
+    public static void main(String[] args) {
+        int a,b;
+
+        a = 6;
+        b = 6;
+    
+        String resultado = a==b ? "verdadeiro" : "falso";
+            
+        // int resultado = a == b ? 1 :0;
+
+        //String resultado = "";
+       //if(a==b)
+           //resultado = "verdadeiro";
+        //else 
+           //resultado = "falso";
+
+        System.out.println(resultado);
+    }
+
+}
