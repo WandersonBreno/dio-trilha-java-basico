fafe8d2b2f9ce4e139c335a5528386b6f762ff7f
Wanderson <wanderbreno2@gmail.com>
2024-06-21 21:56:32 -0300

    rep,un,boo

---
+++ tipos-variaveis/src/booleanos.java
@@ -0,0 +1,12 @@
+public class booleanos {
+
+    public static void main(String [] args){
+        boolean variavel = true;
+
+        //System.out.println(!variavel);
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+
+}
